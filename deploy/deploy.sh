20dc3b18885c2951a0e45cd52e9c56b27c735d67
sgs921107 <757513128@qq.com>
2022-04-08 17:23:14 +0800

    optimize deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,12 +7,13 @@
 #!/bin/bash
 
 # ===================run the script with root user=================================
-# 1.首先根据conf目录下conf demo生成自己的配置文件
+# 1.首先根据conf目录下conf demo生成自己的配置文件(不启动的服务忽略)
 #   conf/my.cnf.demo -> /etc/dbs/my.cnf
 #   conf/redis.conf.demo -> /etc/dbs/redis.conf
 #   conf/elasticsearch0.yml.demo -> /etc/dbs/elasticsearch0.yml
 #   conf/env_demo -> /etc/dbs/.env
-# 3.使用root用户执行此脚本
+# 2.使用root用户执行此脚本
+#   sudo sh deploy.sh
 # 
 # expmple cmd:
 # set -ex \
@@ -34,36 +35,15 @@ sh install_docker.sh || { echo "部署失败: 安装docker失败,请检查是否
 # 将.env链接至部署目录
 ln -s /etc/dbs/.env $HOST_PROJECT_DIR/deploy/.env
 
+
 # 启动服务
-for service in $services
+for service in mysql redis es es_head mongo rabbitmq
 do
-    # 创建服务日志目录, 并添加写权限
-    service_log_dir="$DBS_LOG_DIR/$service"
-    if [ ! -d "$service_log_dir" ]
+    switch=`echo service_$service | tr a-z A-Z`
+    if [ "$(eval echo \$$switch)" == "1" ]
     then
-        mkdir -p $service_log_dir
+        docker-compose up -d $service \
+        && { echo "启动"$service"服务成功"; docker-compose logs  --tail 10 $service; } \
+        || echo "启动服务"$service"失败!!!"
     fi
-    chmod a+w $service_log_dir
-
-    # 启动服务
-    docker-compose up -d $service
-    # 将服务的宿主机端口添加至防火墙
-    # upper_service=${service^^}
-    # if [ $service = es ]
-    # then
-    #     service_port1=`eval echo '$'"REAL_${upper_service}_TCP_PORT"`
-    #     service_port2=`eval echo '$'"REAL_${upper_service}_HTTP_PORT"`
-    #     firewall-cmd --permanent --add-port=$service_port1/tcp
-    #     firewall-cmd --permanent --add-port=$service_port2/tcp
-    # else
-    #     service_port=`eval echo '$'"REAL_${upper_service}_PORT"`
-    #     # 添加端口到防火墙
-    #     firewall-cmd --permanent --add-port=$service_port/tcp
-    # fi
 done
-
-# 删除产生的<none>镜像
-# docker rmi $(docker images | grep '<none>' | awk '{print $3}')
-
-# 重新加载防火墙
-# firewall-cmd --reload
